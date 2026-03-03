9dd322077e34c205dc104a97c5e15ce9b542050c
Tristan P. <tristanrpatterson@gmail.com>
2026-03-03 12:05:09 -0800

    Define attributes and constructor for Data class
    Added private attributes and constructor for the Data class.

--- src/Data.java
+++ src/Data.java
@@ -1,27 +1,29 @@
 /**
  * Represents one row from your dataset.
- *
- * TODO:
- *  - Rename the class to match your dataset (e.g., Pokemon, StateData, CountryStat)
- *  - Add at least 3 private attributes based on your CSV columns
- *  - Write a constructor that initializes all attributes
- *  - Add getter methods for the attributes you need in your analysis
- *  - Override toString() to display the object's data
- *  - Add Javadoc comments for the class and all methods
  */
 public class Data {
 
-    // TODO: Add at least 3 private attributes
-    // Example:
-    // private String name;
-    // private int population;
-    // private double value;
+    private String name;
+    private String types;
+    private int dexNumber;
 
+   public Pokemon(String name, String types, int dexNumber){
+       this.name = name;
+       this.type1 + this.type2 = types;
+       this.number = dexNumber;
+   }
 
-    // TODO: Create a constructor that takes all attributes as parameters
+public String getName(){
+    return name;
+}
 
+public String getTypes(){
+    return types;
+}
 
-    // TODO: Add getters for attributes you need
+public int getNumber(){
+    return dexNumber;
+}
 
     // TODO: Add other data analysis methods
 
