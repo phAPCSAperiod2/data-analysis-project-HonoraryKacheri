de9bd57948d6b3469af00a98f5bd4db543104ad4
Tristan P. <tristanrpatterson@gmail.com>
2026-03-04 20:47:11 +0000

    modified:   bin/App.class 	modified:   src/App.jav

--- src/App.java
+++ src/App.java
@@ -2,23 +2,26 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         String filename = "/workspaces/data-analysis-project-HonoraryKacheri/pokemon.csv";
-        File pokemonData = new File(filename);
-
-        Scanner scan = new Scanner();
-        int pokedexNumber = 1;
-        for (int[] pokedexNumber : pokemonData){
-            String pokemonName = "";
-            String type1 = "";
-            String type2 = "";
-            int dexNumber = 0;
-            for (int col : row){
-                pokemonName = pokemonData[pokedexNumber][col];
-    }
-
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                String pokemonName = "";
+                String type1 = "";
+                String type2 = "";
+                int dexNumber = 0;
+                for (String col : row) {
+                    pokemonName = row[0];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
