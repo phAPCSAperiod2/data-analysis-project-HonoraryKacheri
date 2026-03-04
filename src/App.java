b421da6338953fd0fbbf72e64e62a7fe443de1de
Tristan P. <tristanrpatterson@gmail.com>
2026-03-04 20:42:53 +0000

    modified:   bin/App.class 	modified:   bin/Data.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
-        // TODO: Update this with your CSV file path
         String filename = "/workspaces/data-analysis-project-HonoraryKacheri/pokemon.csv";
         File pokemonData = new File(filename);
 
@@ -18,29 +17,8 @@ public class App {
             int dexNumber = 0;
             for (int col : row){
                 pokemonName = pokemonData[pokedexNumber][col];
-                
-        // - Split each line by commas
-        // - Convert text to numbers when needed
-        // - Create new Data objects
-        // - Add to your array
-
-
-        // TODO: Call your analysis methods
-        // Example:
-        // double maxValue = findMaxValue(dataList);
-        // double average = computeAverageValue(dataList);
-
-
-        // TODO: Print insights
-        // - Number of rows loaded
-        // - Min, max, average, or any other findings
-        // - Final answer to your guiding question
-
-
-        // OPTIONAL TODO:
-        // Add user interaction:
-        // Ask the user what kind of analysis they want to see
     }
 
-
+        }
+    }
 }
