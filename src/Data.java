5ef607ccbbd5623a2de4c423af282976ba76d0a3
Tristan P. <tristanrpatterson@gmail.com>
2026-03-04 20:44:54 +0000

    modified:   bin/Data.class 	modified:   src/Data.java

--- src/Data.java
+++ src/Data.java
@@ -6,11 +6,6 @@ public class Data {
     private String name;
     private String types;
     private int dexNumber;
-
-   public void Pokemon(String name, String types, int dexNumber){
-       this.name = name;
-       types = this.type1 + " " + this.type2;
-       this.number = dexNumber;
    }
 
 public String getName(){
